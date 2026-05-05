79f75863c1d4b8f14df592168d83cd01f4968d67
Nils Moreau--Thomas <terminatorletrol@gmail.com>
2026-05-05 23:19:44 +0200

    layer 2 of train

--- src/draw_scene.cpp
+++ src/draw_scene.cpp
@@ -11,8 +11,22 @@ float rr = 1;
 
 //for train
 const float btwn_rails = 10.f - (2*3 + 2*sr);
-const float wheel_gap = 2.f;
-const float wheel_wedge_gap;
+const float btwn_rails_out = btwn_rails + 2*sr;
+const float wheel_support_size = 3.0;
+const float wheel_wedge_size = 0.25;
+const float wheel_guard_size = 0.75;
+const float under_wedge_size = 2.5;
+//accumulate size of stuff used for 1st layer
+const float body_size = 
+    3 * wheel_guard_size 
+    + 4 * wheel_wedge_size 
+    + 2 * wheel_support_size
+    + under_wedge_size;
+const float main_body_height = 2.25;
+const float body_rep[2] = {0.8f,0.2f}; //repartition between wedge and main body (%)
+const float main_body_size = body_rep[0] * body_size;
+const float wedge_body_size = body_rep[1] * body_size;
+
 
 GLBI_Engine myEngine;
 GLBI_Set_Of_Points somePoints(3);
@@ -102,18 +116,52 @@ void drawGround(int size) {
     drawShapeWithColor(ground,0.2,0.0,0.0);
 }
 
-void drawTrain(float time) {
-    //TODO: draw the train
-    //TODO: draw according to time, following path (later)
 
 
-    //NOTE: wheels
+//////////////////////////////////////
+/* Train */
+//////////////////////////////////////
 
-    
-    
+
+
+void drawTrainWheelWedge() {
     myEngine.mvMatrixStack.pushMatrix();
-        drawTrainWheels();
-        
+        moveOrigin(wheel_wedge_size/2, btwn_rails_out/2, sr*0.75);
+        scaleOrigin(wheel_wedge_size, btwn_rails_out, sr);
+        drawShapeWithColor(cube,1,0,0);
+        myEngine.mvMatrixStack.popMatrix();
+}
+
+void drawTrainWheelGuard() {
+    myEngine.mvMatrixStack.pushMatrix();
+        moveOrigin(wheel_guard_size/2, btwn_rails_out/2, sr*0.75);
+        scaleOrigin(wheel_guard_size, btwn_rails_out, sr);
+        drawShapeWithColor(cube,0,1,0);
+        myEngine.mvMatrixStack.popMatrix();
+}
+
+void drawTrainUnderWedge() {
+    myEngine.mvMatrixStack.pushMatrix();
+        moveOrigin(under_wedge_size/2, btwn_rails_out/2, sr*0.625);
+        scaleOrigin(under_wedge_size, btwn_rails_out, sr*0.75);
+        drawShapeWithColor(cube,1,1,1);
+        myEngine.mvMatrixStack.popMatrix();
+}
+
+void drawTrainMainBody() {
+    myEngine.mvMatrixStack.pushMatrix();
+        moveOrigin(main_body_size/2,btwn_rails_out/2,main_body_height/2);
+        scaleOrigin(main_body_size, btwn_rails_out, main_body_height);
+        drawShapeWithColor(cube,1,0.5,0);
+        myEngine.mvMatrixStack.popMatrix();
+}
+
+void drawTrainWedgeBody() {
+    myEngine.mvMatrixStack.pushMatrix();
+        moveOrigin(wedge_body_size/2,btwn_rails_out/2,main_body_height/2);
+        scaleOrigin(wedge_body_size, btwn_rails_out, main_body_height);
+        drawShapeWithColor(cube,1,0,0.5);
+        myEngine.mvMatrixStack.popMatrix();
 }
 
 void drawTrainWheels() {
@@ -127,7 +175,7 @@ void drawTrainWheels() {
 
         //wh2
         myEngine.mvMatrixStack.pushMatrix();
-            moveOrigin(wheel_gap + sr/2, 0, sr/2);
+            moveOrigin(wheel_support_size - sr/2, 0, sr/2);
             scaleOrigin(sr, sr, sr);
             drawShapeWithColor(cylinder,0,0.5,1);
             myEngine.mvMatrixStack.popMatrix();
@@ -141,7 +189,7 @@ void drawTrainWheels() {
 
         //wh4
         myEngine.mvMatrixStack.pushMatrix();
-            moveOrigin(wheel_gap + sr/2, btwn_rails + sr, sr/2);
+            moveOrigin(wheel_support_size - sr/2, btwn_rails + sr, sr/2);
             scaleOrigin(sr, sr, sr);
             drawShapeWithColor(cylinder,0,0.5,1);
             myEngine.mvMatrixStack.popMatrix();
@@ -149,13 +197,80 @@ void drawTrainWheels() {
         //base
         myEngine.mvMatrixStack.pushMatrix();
             moveOrigin(0, sr*2, sr*0.75);
-            moveOrigin((wheel_gap + sr)/2,0,0);
-            scaleOrigin(wheel_gap + sr, btwn_rails, sr);
+            moveOrigin(wheel_support_size/2,0,0);
+            scaleOrigin(wheel_support_size, btwn_rails, sr);
             drawShapeWithColor(cube,0,0,1);
             myEngine.mvMatrixStack.popMatrix();
         //end
         myEngine.mvMatrixStack.popMatrix();
 }
+
+void drawTrain(float time) {
+    //TODO: draw the train
+    //TODO: draw according to time, following path (later)
+    //NOTE: use triangles instead of cubes (have to create it in basic_mesh.hpp)
+    
+    //1st layer
+    myEngine.mvMatrixStack.pushMatrix();
+        //guard 1
+        drawTrainWheelGuard();
+        moveOrigin(wheel_guard_size,0,0);
+
+        drawTrainWheelWedge();
+        moveOrigin(wheel_wedge_size,0,0);
+
+        //wheels 1
+        drawTrainWheels();
+        moveOrigin(wheel_support_size,0,0);
+        
+        //guard 2
+        drawTrainWheelWedge();
+        moveOrigin(wheel_wedge_size,0,0);
+
+        drawTrainWheelGuard();
+        moveOrigin(wheel_guard_size,0,0);
+
+        drawTrainWheelWedge();
+        moveOrigin(wheel_wedge_size,0,0);
+
+        //wheels 2
+        drawTrainWheels();
+        moveOrigin(wheel_support_size,0,0);
+
+        //guard 3
+        drawTrainWheelWedge();
+        moveOrigin(wheel_wedge_size,0,0);
+
+        drawTrainWheelGuard();
+        moveOrigin(wheel_guard_size,0,0);
+
+        //under wedge
+        drawTrainUnderWedge();
+        moveOrigin(under_wedge_size,0,0);
+
+        myEngine.mvMatrixStack.popMatrix();
+
+    //2nd layer
+    myEngine.mvMatrixStack.pushMatrix();
+        moveOrigin(0,0,sr);
+
+        //main body
+        drawTrainMainBody();
+        moveOrigin(main_body_size,0,0);
+
+        //wedge body
+        drawTrainWedgeBody();
+        moveOrigin(wedge_body_size,0,0);
+
+        myEngine.mvMatrixStack.popMatrix();
+}
+
+
+//////////////////////////////////////
+/* Rails */
+//////////////////////////////////////
+
+
 
 void drawRailCurve(float orientation) {
 	// TODO: orientate accordingly
@@ -164,6 +279,13 @@ void drawRailCurve(float orientation) {
 void drawRailStraight(float orientation) {
 	// TODO: orientate accordingly
 }
+
+
+//////////////////////////////////////
+/* Scene drawing */
+//////////////////////////////////////
+
+
 
 void drawScene(float time, Railways* railways) {
 	glPointSize(10.0);
