1e21c83d4c3a855f5aa7a3d3db0cc876a1a06708
Yanis-Wg <yan.nw.77185@gmail.com>
2026-06-05 11:53:39 +0200

    Ajout des lumières - Fini

--- src/lighting/lighting.cpp
+++ src/lighting/lighting.cpp
@@ -2,8 +2,32 @@
 
 void handleMainLighting(double time) {
     //TODO: do the "sun" like lightning, maybe animate it with the time
+    const float angle{15.};
+    const float hauteur{10.};
+    Vector4D light_pos(0,20,0,0);
+    myEngine.setLightPosition({
+        sin(time) * angle,
+        hauteur,
+        cos(time) * angle,
+        0.0f
+    }, 0);
+    // couleur light
+    Vector3D light_intensity(1,0.7,0.7);
+    myEngine.setLightIntensity(light_intensity);
+
+    Vector3D light_att(1,0,0);
+    myEngine.setAttenuationFactor(light_att);
+
+    myEngine.setShininess(24.);
+
+    Vector3D reflect_col(1,1,1);
+    myEngine.setSpecularColor(reflect_col);
+
 };
 
 void handleTrainLighting(float x, float y, float z) {
     //TODO: place the train light at the given coords (it's the front of train)
+    Vector4D position(x,y,z,1.);
+    Vector3D couleur(1.,0.5,0.7);
+    myEngine.addALight(position,couleur);
 }
