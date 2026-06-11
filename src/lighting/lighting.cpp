d5ef84249880a80f5dce9f4e03cd538bc3c5a3e5
Nils Moreau--Thomas <terminatorletrol@gmail.com>
2026-06-11 15:28:43 +0200

    fixed light for more visibility

--- src/lighting/lighting.cpp
+++ src/lighting/lighting.cpp
@@ -3,29 +3,31 @@
 bool lightInitialized = false;
 
 void initMainLighting() {
-    Vector3D light_attenuation(0,0,0); // puissance
     Vector3D color(1,1,0); // couleur
-
-    myEngine.setSpecularColor(color);
-    myEngine.setAttenuationFactor(light_attenuation);
-    myEngine.setShininess(0.0f);
+    
     myEngine.setLightIntensity(color, 0);
 };
 
 void initTrainLighting() {
     Vector3D light_attenuation(1,0,0); // puissance
-    Vector3D color(1,0,0); // couleur
-
-    myEngine.setSpecularColor(color);
-    myEngine.setAttenuationFactor(light_attenuation);
-    myEngine.setShininess(0.0f);
+    Vector3D color(2,0,0); // couleur
+    
     myEngine.addALight({0,0,0,1}, color);
 };
 
 void initLights() {
+    Vector3D attenuation(1.0f,1.0f,1.0f);
+    Vector3D quantity(1.0f,1.0f,1.0f);
+    float shininess = 0.0f;
+
     //if light not placed yet AND we are on the shader that use lighting
     if (!lightInitialized && myEngine.currentShader == 1) {
         lightInitialized = true;
+
+        myEngine.setAttenuationFactor(attenuation);
+        myEngine.setShininess(shininess);
+        myEngine.setSpecularColor(quantity);
+
         initMainLighting();
         initTrainLighting();
     }
@@ -35,11 +37,13 @@ void handleMainLighting(double time) {
     const float radius{10.0f};
     const float height{25.0f};
     const float speed{0.5};
+    const float x_offset = 0.0f;
+    const float z_offset = -50.f;
 
     myEngine.setLightPosition({
-        (float)(sin(time * speed) * radius),
+        (float)(sin(time * speed) * radius) + x_offset,
         height,
-        (float)(cos(time * speed) * radius),
+        (float)(cos(time * speed) * radius) + z_offset,
         0.0f
     }, 0);
 };
